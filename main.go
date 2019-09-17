fa4b0fe40fc818260bb2121eb8f00c816e2fab5c
vamshi.nannuta <vamshi.nannuta@nutanix.com>
2019-09-17 15:07:48 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+type Set map[int]bool
+
+func (s Set) Union(s2 Set) Set {
+	for k, _ := range s2 {
+		if _, ok := s[k]; !ok {
+			s[k] = true
+		}
+	}
+	return s
+}
+
+func (s Set) Intersection(s2 Set) Set {
+	for k, _ := range s {
+		if _, ok := s2[k]; !ok {
+			delete(s, k)
+		}
+	}
+	return s
+}
+
+func (s Set) Difference(s2 Set) Set {
+	for k, _ := range s {
+		if _, ok := s2[k]; ok {
+			delete(s, k)
+		}
+	}
+	return s
+}
+
+func main() {
+
+}
